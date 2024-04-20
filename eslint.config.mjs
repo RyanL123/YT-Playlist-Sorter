18f70ec3e5da49442c471770ca6219d3455c5438
Ryan Li <ryan.lichangrui@gmail.com>
2024-04-20 10:14:23 -0400

    ESLint config

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  pluginReactConfig,
+  eslintConfigPrettier,
+  {
+    ignores: ["build/*"],
+  },
+];
