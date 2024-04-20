7ee7a3588016f393beb0229edaa3242619da35cb
Ryan <ryan.lichangrui@gmail.com>
2024-04-20 18:09:39 -0400

    YT-17: Migrate Video.js (#12)

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,10 +1,22 @@
 import { Box, Link, useMediaQuery } from "@mui/material";
 import Typography from "@mui/material/Typography";
-import PropTypes from "prop-types";
-import React from "react";
+import * as React from "react";
 import { convertISOtoString } from "../util/dateUtil";
 
-const Video = (props) => {
+const Video = ({
+  stats,
+}: {
+  stats: {
+    link: string;
+    title: string;
+    channel: string;
+    views: number;
+    likes: number;
+    uploadDate: string;
+    duration: number;
+    thumbnail: string;
+  };
+}) => {
   const mobile = useMediaQuery("(max-width:1000px)");
   return (
     <Box
@@ -16,56 +28,41 @@ const Video = (props) => {
     >
       <Box display="flex" flexDirection="column">
         <Typography variant="h5" color="text.primary">
-          <Link href={props.stats.link} target="_blank">
-            {props.stats.title}
+          <Link href={stats.link} target="_blank">
+            {stats.title}
           </Link>
         </Typography>
         <Typography variant="h6" color="text.primary">
-          {props.stats.channel}
+          {stats.channel}
         </Typography>
         <Typography variant="h6" color="text.primary">
-          Views:{" "}
-          {props.stats.views.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+          Views: {stats.views.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
         </Typography>
         <Typography variant="h6" color="text.primary">
-          Likes:{" "}
-          {props.stats.likes.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+          Likes: {stats.likes.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
         </Typography>
         <Typography variant="h6" color="text.primary">
-          Upload Date: {convertISOtoString(props.stats.uploadDate)}
+          Upload Date: {convertISOtoString(stats.uploadDate)}
         </Typography>
         <Typography variant="h6" color="text.primary">
           Duration:{" "}
-          {new Date(props.stats.duration * 1000).toISOString().substr(11, 8)}
+          {new Date(stats.duration * 1000).toISOString().substr(11, 8)}
         </Typography>
       </Box>
       <Box>
-        <Link href={props.stats.link} target="_blank">
+        <Link href={stats.link} target="_blank">
           <img
-            src={props.stats.thumbnail}
+            src={stats.thumbnail}
             style={{
               width: mobile ? "100%" : "300px",
               height: "auto",
             }}
-            alt={props.stats.title}
+            alt={stats.title}
           />
         </Link>
       </Box>
     </Box>
   );
-};
-
-Video.propTypes = {
-  stats: PropTypes.shape({
-    title: PropTypes.string,
-    thumbnail: PropTypes.string,
-    link: PropTypes.string,
-    channel: PropTypes.string,
-    likes: PropTypes.number,
-    uploadDate: PropTypes.string,
-    views: PropTypes.number,
-    duration: PropTypes.number,
-  }),
 };
 
 export default Video;
